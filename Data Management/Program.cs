93a0c459ceda10e6b8bb47b33caee6c859f6fce8
Matthew Mc Girl <S00144119@mail.itsligo.ie>
2020-09-02 13:16:33 +0100

    creating database

---
+++ Data Management/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MatthewMcGirl_S00144119;
+
+namespace Data_Management
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CompData db = new CompData();
+
+            using(db)
+            {
+                Computer c1 = new Computer("Acer 714", 599.99m, "ChromeOS", "128GB eMMC", "8GB", "Intel Core i3", "/images/acer_logo.png", "/images/acer.png");
+
+                Computer c2 = new Computer("Hp Pavilion 14", 539m, "Windows 10", "256GB SSD", "8GB", "Intel Core i3", "/images/hp_logo.png", "/images/hp_pavilion.png");
+            }
+        }
+    }
+}
--- MatthewMcGirl_S00144119/Computer.cs
+++ MatthewMcGirl_S00144119/Computer.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,21 +10,47 @@ namespace MatthewMcGirl_S00144119
 {
     public class Computer
     {
-        public int compID { get; set; }
+        public Computer(string v1, decimal v2, string v3, string v4, string v5, string v6, string v7, string v8)
+        {
+            this.compName = v1;
+            this.compPrice = v2;
+            this.compOS = v3;
+            this.compHarddrive = v4;
+            this.compMemory = v5;
+            this.compProcessor = v6;
+            this.manfImage = v7;
+            this.compImage = v8;
+        }
+
+        public string compID { get; set; }
         public string compName { get; set; }
         public decimal compPrice { get; set; }
         public string compOS { get; set; }
-        public string manfImage { get; set; }
-        public string compImage { get; set; }
         public string compHarddrive { get; set; }
         public string compMemory { get; set; }
-        public string compProcessor {get; set;}
-        public decimal finaldecreasedPrice { get; set; }
+        public string compProcessor { get; set; }
+        public string manfImage { get; set; }
+        public string compImage { get; set; }
+
+
+
+        public virtual List<Computer> Computers { get; set; }
 
         public void DecreasePrice(decimal amount)
         {
             decimal discount20 = (80 / 100);
             decimal finaldecreasedPrice = compPrice * discount20;
         }
+
+
+
+
+        
+    }
+
+    public class CompData : DbContext
+    {
+        public CompData() : base("MyCompData") { }
+        public DbSet<Computer> Computers { get; set; }
     }
 }
